64a2a8595d528aab5c9efa60f5182157be48a5fc
Tony <tuan.nguyen@orica.com>
2023-08-12 19:18:17 +1000

    Fourth commit. Modified some lines.

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 char name[4] = "Tuan";
 
 int main() {
-    printf("Hello, %s!\n", name);
+    printf("Hello, readers!\n", name);
     printf("%s is an electronic engineer!\n", name);
-    printf("%s is happy.\n", name);
+    printf("%s is not so happy.\n", name);
     return 0;
 }
